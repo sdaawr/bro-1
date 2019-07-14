9d2c6566a4073288630d6f846fcd0388ac929850
sdaawr <45720212+sdaawr@users.noreply.github.com>
2019-07-14 08:26:54 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`=bc |Love this server`,"http://twitch.tv/Death ")
+client.user.setGame(`=bc | Love this server ♥`,"http://twitch.tv/Death ")
 client.user.setStatus("dnd")
 });
 
